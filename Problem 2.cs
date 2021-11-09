d9157a0fb3fb1359794b7d4ec290997dc0ab7536
Vonfagelsang <89455450+Vonfagelsang@users.noreply.github.com>
2021-11-09 15:42:06 +0100

    Add files via upload

---
+++ Problem 2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+namespace proggramerings_olympiaden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+Console.WriteLine("Ord:");
+string Ord = Console.ReadLine();
+Console.WriteLine("Mening:");
+string Mening = Console.ReadLine();  
+  
+string result = string.Join(" ", Mening  
+  
+.Split(' ')  
+  
+.Select(x => new String(x.Reverse().ToArray())));  
+  
+
+ int temp;
+
+            string s = result;
+
+            string[] a = s.Split(' ');
+
+            int k = a.Length - 1;
+
+            temp = k;
+
+           for (int i = k; temp >= 0; k--)
+
+            {
+
+                Console.Write(a[temp] + "" + ' ');
+
+                --temp;
+
+            }
+
+        }
+    }
+}
